e4b685f5ace7e15ad49d2dbc491636340679db9a
dflk <904885508@qq.com>
2023-01-28 00:14:59 +0800

    给欢迎页面添加动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,6 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { useLocation, useOutlet } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
+import type { ReactNode } from 'react'
+import { useRef } from 'react'
+
 export const WelcomeLayout: React.FC = () => {
-  return <div>
-    <Outlet />
-  </div>
+  const map = useRef<Record<string, ReactNode>>({})
+  const location = useLocation()
+  const pathname = location.pathname
+
+  const outlet = useOutlet()
+  map.current[pathname] = outlet
+  const transitions = useTransition(pathname, {
+    from: { transform: pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
+    enter: { transform: 'translateX(0%)' },
+    leave: { transform: 'translateX(-100%)' },
+    config: { duration: 300 }
+  })
+
+  return transitions((style, pathname) => (
+    <animated.div
+    style={style} key={pathname}
+  ><div style={{ textAlign: 'center' }}> {map.current[pathname]}</div></animated.div>
+  ))
 }
--- src/pages/Welcome3.tsx
+++ src/pages/Welcome3.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 import { NavLink } from 'react-router-dom'
 export const Welcome3: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}> 1 <NavLink to="/welcome/4">下一页</NavLink> </div>
+    <div style={{ border: '1px solid red' }}> 3 <NavLink to="/welcome/4">下一页</NavLink> </div>
   )
 }
