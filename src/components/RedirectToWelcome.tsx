3a815e36884fa75b0d90681f3b98134f87f639e8
dflk <904885508@qq.com>
2023-01-27 16:22:40 +0800

    将404改为重定向到欢迎页

---
+++ src/components/RedirectToWelcome.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function ErrorPage() {
+  const nav = useNavigate()
+  useEffect(() => {
+    nav('welcome/1')
+  })
+}
