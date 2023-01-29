bd323b3d9ca093bf48cf2bcf2b01b37e524ad0e2
dflk <904885508@qq.com>
2023-01-29 16:26:17 +0800

    添加使用unocss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import 'virtual:uno.css'
 const root = document.getElementById('root') as HTMLElement
 const rootDOM = ReactDOM.createRoot(root)
 rootDOM.render(
