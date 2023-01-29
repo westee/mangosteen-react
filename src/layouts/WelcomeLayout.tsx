e0d85d6616f01f179bdd074369a5c1247d338db3
dflk <904885508@qq.com>
2023-01-29 23:34:58 +0800

    完成第一个欢迎页html

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,25 +1,48 @@
-import { useLocation, useOutlet } from 'react-router-dom'
-import { animated, useTransition } from '@react-spring/web'
-import type { ReactNode } from 'react'
-import { useRef } from 'react'
+import {Link, useLocation, useOutlet} from 'react-router-dom'
+import {animated, useTransition} from '@react-spring/web'
+import type {ReactNode} from 'react'
+import {useRef} from 'react'
+import logo from '../assets/images/mangosteen.svg'
+
+const routeReflect = {
+    '/welcome/1': '/welcome/2',
+    '/welcome/2': '/welcome/3',
+    '/welcome/3': '/welcome/4',
+    '/welcome/4': '/xxx',
+}
 
 export const WelcomeLayout: React.FC = () => {
-  const map = useRef<Record<string, ReactNode>>({})
-  const location = useLocation()
-  const pathname = location.pathname
+    const map = useRef<Record<string, ReactNode>>({})
+    const location = useLocation()
+    const outlet = useOutlet()
+    map.current[location.pathname] = outlet
+    const transitions = useTransition(location.pathname, {
+        from: {transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)'},
+        enter: {transform: 'translateX(0%)'},
+        leave: {transform: 'translateX(-100%)'},
+        config: {duration: 300}
+    })
 
-  const outlet = useOutlet()
-  map.current[pathname] = outlet
-  const transitions = useTransition(pathname, {
-    from: { transform: pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
-    enter: { transform: 'translateX(0%)' },
-    leave: { transform: 'translateX(-100%)' },
-    config: { duration: 300 }
-  })
+    return (<div>
+        <header>
+            <img src={logo} alt=""/>
+            <h1>山竹记账</h1>
+        </header>
+        <main>
+            {
+                transitions((style, pathname) => (
+                    <animated.div
+                        style={style} key={pathname}
+                    >
+                        <div style={{textAlign: 'center'}}> {map.current[pathname]}</div>
+                    </animated.div>
+                ))
+            }
+        </main>
 
-  return transitions((style, pathname) => (
-    <animated.div
-    style={style} key={pathname}
-  ><div style={{ textAlign: 'center' }}> {map.current[pathname]}</div></animated.div>
-  ))
+        <footer>
+             <Link to={routeReflect[location.pathname]}>下一页</Link>
+             <Link to={'/'}>跳过</Link>
+        </footer>
+    </div>)
 }
--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
-import { NavLink } from 'react-router-dom'
+import pig from '../assets/images/pig.svg'
 export const Welcome1: React.FC = () => {
   return (
-    <div flex b-1 b-blue text="sm red"> 1 <NavLink to="/welcome/2">下一页</NavLink> </div>
+    <div flex b-1 b-blue text="sm red">
+      <img src={pig} alt=""/>
+      <h1>
+        会挣钱 <br/>
+        还会省钱
+      </h1>
+    </div>
   )
 }
