32b817f9dc643c146419e301bbf338fa1988aa32
dflk <904885508@qq.com>
2023-01-30 21:38:44 +0800

    解决WelcomeLayouts的ts报错

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,47 +1,47 @@
-import {Link, useLocation, useOutlet} from 'react-router-dom'
-import {animated, useTransition} from '@react-spring/web'
-import type {ReactNode} from 'react'
-import {useRef,useState} from 'react'
+import { Link, useLocation, useOutlet } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
+import type { ReactNode } from 'react'
+import { useRef, useState } from 'react'
 import logo from '../assets/images/mangosteen.svg'
 
-const routeReflect = {
-    '/welcome/1': '/welcome/2',
-    '/welcome/2': '/welcome/3',
-    '/welcome/3': '/welcome/4',
-    '/welcome/4': '/xxx',
+const routeReflect: Record<string, string> = {
+  '/welcome/1': '/welcome/2',
+  '/welcome/2': '/welcome/3',
+  '/welcome/3': '/welcome/4',
+  '/welcome/4': '/xxx',
 }
 
 export const WelcomeLayout: React.FC = () => {
-    const map = useRef<Record<string, ReactNode>>({})
-    const location = useLocation()
-    const outlet = useOutlet()
-    map.current[location.pathname] = outlet
-    const [extraStyle, setExtraStyle] = useState({ position: 'relative' })
-    const transitions = useTransition(location.pathname, {
-        from: {transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)'},
-        enter: {transform: 'translateX(0%)'},
-        leave: {transform: 'translateX(-100%)'},
-        onStart: () => {
-            setExtraStyle({ position: 'absolute' })
-        },
-        onRest: () => {
-            setExtraStyle({ position: 'relative' })
-        },
-        config: {duration: 3000},
-    })
+  const map = useRef<Record<string, ReactNode>>({})
+  const location = useLocation()
+  const outlet = useOutlet()
+  map.current[location.pathname] = outlet
+  const [extraStyle, setExtraStyle] = useState<{ position: 'realative' | 'absolute' }>({ position: 'relative' })
+  const transitions = useTransition(location.pathname, {
+    from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
+    enter: { transform: 'translateX(0%)' },
+    leave: { transform: 'translateX(-100%)' },
+    onStart: () => {
+      setExtraStyle({ position: 'absolute' })
+    },
+    onRest: () => {
+      setExtraStyle({ position: 'relative' })
+    },
+    config: { duration: 3000 },
+  })
 
-    return (<div h-screen flex flex-col align-center bg="#8f4cd7" pb-16px>
+  return (<div h-screen flex flex-col align-center bg="#8f4cd7" pb-16px>
         <header p-t-66px flex flex-col items-center>
             <img w-64px src={logo} alt=""/>
-            <h1 text={"#d4d4ee"}>山竹记账</h1>
+            <h1 text={'#d4d4ee'}>山竹记账</h1>
         </header>
         <main shrink-1 grow-1 relative>
             {
                 transitions((style, pathname) => (
                     <animated.div w="100%" h="100%" grow-1 p-16px flex
-                        style={{...style, ...extraStyle}} key={pathname}
+                        style={{ ...style, ...extraStyle }} key={pathname}
                     >
-                        <div  grow-1 bg-white flex justify-center items-center rounded-8px> {map.current[pathname]}</div>
+                        <div grow-1 bg-white flex justify-center items-center rounded-8px> {map.current[pathname]}</div>
                     </animated.div>
                 ))
             }
