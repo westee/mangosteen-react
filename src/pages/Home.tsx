0042cb7ab087b50df0d3971a4ae9e8e5d0494cc9
dflk <904885508@qq.com>
2023-02-01 22:09:42 +0800

    增加swr和axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react'
+import useSWR from 'swr'
+import axios from 'axios'
+
+const fetcher = (path: string) => axios.get<{ message: string }>(path).then(res => res.data)
+
 export const Home: React.FC = () => {
-  return (
-    <div>
-      home
-    </div>
-  )
+  const { data, error, isLoading, mutate } = useSWR('http://121.196.236.94:3000', fetcher)
+
+  const a = () => {
+    const data = await axios.post('xxx') // message: 'hi'
+    mutate(data)
+  }
+  if (error)
+    return <div>failed to load</div>
+  if (isLoading)
+    return <div>loading...</div>
+
+  return <div>hello {data?.message}!</div>
 }
